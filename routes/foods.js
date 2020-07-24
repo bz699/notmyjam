925123c300302a61c42440e4456824fd2de4252b
koob <zeebha@gmail.com>
2020-07-25 00:18:39 +0200

    fix/resultS in queries

--- routes/foods.js
+++ routes/foods.js
@@ -4,11 +4,11 @@ const connection  = require('../config');
 
 // GET ALL foods
 router.get('/', (req, res) => {
-  connection.query('SELECT * FROM food', (err, result) => {
+  connection.query('SELECT * FROM food', (err, results) => {
     if(err) {
       res.status(500).send(err)
     } else {
-      res.status(200).json(result)
+      res.status(200).json(results)
     }
   })
 });
@@ -17,13 +17,13 @@ router.get('/', (req, res) => {
 router.get('/:idFood', (req, res) => {
   const { idFood } = req.params;
 
-  connection.query('SELECT * FROM food WHERE id = ?', [idFood], (err, result) => {
+  connection.query('SELECT * FROM food WHERE id = ?', [idFood], (err, results) => {
       if (err) {
           res.status(500).send(err)
       } else if (results.length === 0) {
           res.status(400).send('this food does\'nt exist')
       } else {
-          res.status(200).json(result)
+          res.status(200).json(results)
       }
   })
 });
@@ -61,13 +61,13 @@ router.put('/:idFood', (req, res) => {
 router.delete('/:idFood', (req, res) => {
   const { idFood } = req.params;
 
-  connection.query('DELETE FROM food WHERE id = ?', [idFood], (err, result) => {
+  connection.query('DELETE FROM food WHERE id = ?', [idFood], (err, results) => {
       if (err) {
           res.status(500).send(err)
       } else if (results.length === 0) {
           res.status(400).send('can\'t delete this food')
       } else {
-          res.status(200).json(result)
+          res.status(200).json(results)
       }
   })
 });
